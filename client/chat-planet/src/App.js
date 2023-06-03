cb7c8b003c86bae55d6e1af97951bc4dc01576f6
jcoleonline <jcoleonlinellc@gmail.com>
2023-06-03 10:16:13 -0500

    created a login page

--- client/chat-planet/src/App.js
+++ client/chat-planet/src/App.js
@@ -4,6 +4,7 @@ import Dashboard from './Components/Dashboard/Dashboard';
 import './App.css';
 import Register from './Components/Register/Register';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Login from './Components/Login/Login';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard/>}/>
           <Route path="/register" element={<Register/>}/>
+          <Route path="/login" element={<Login/>}/>
         </Routes>
       </BrowserRouter>
     </div>
